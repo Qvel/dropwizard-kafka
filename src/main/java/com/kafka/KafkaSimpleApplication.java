efbabe2beafd5722829e2ac258de51226b4fc411
szhura <v-szhuravlov@vrbo.com>
2020-01-12 20:41:54 +0200

    app with consumer and producer (with end-points)

--- src/main/java/com/kafka/KafkaSimpleApplication.java
+++ src/main/java/com/kafka/KafkaSimpleApplication.java
@@ -8,19 +8,13 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.concurrent.ExecutorService;
-import java.util.stream.IntStream;
 
 public class KafkaSimpleApplication extends Application<KafkaSimpleConfiguration> {
 
@@ -46,48 +40,24 @@ public class KafkaSimpleApplication extends Application<KafkaSimpleConfiguration
         // TODO: implement application
         final KafkaResource resource = new KafkaResource(
             configuration.getTemplate(),
-            configuration.getDefaultName()
+            configuration.getDefaultName(),
+            buildProducer(configuration),
+            buildConsumer(configuration)
         );
         final TemplateHealthCheck healthCheck =
             new TemplateHealthCheck(configuration.getTemplate());
         environment.healthChecks().register("template", healthCheck);
         environment.jersey().register(resource);
-
-        runKafkaTest(configuration,environment);
-
     }
 
-
-    public void runKafkaTest(KafkaSimpleConfiguration configuration, Environment environment) {
-        final ExecutorService executorService = environment
-            .lifecycle()
-            .executorService("kafka-threads")
-            .minThreads(2)
-            .maxThreads(10)
-            .build();
-
-        executorService.execute(() -> {
-            final Producer<String, String> producer = new KafkaProducer<>(configuration.getProducer());
-            int i = 0;
-            try {
-                Thread.sleep(5000);
-                String s = "Hello from qvel";
-                producer.send(new ProducerRecord<>("my-topic", s, s));
-
-            } catch (InterruptedException e) {
-                LOGGER.info("Producer interrupted");
-                producer.close();
-            }
-        });
-
-        executorService.execute(() -> {
-            final Consumer<String, String> consumer = new KafkaConsumer<>(configuration.getConsumer());
-            consumer.subscribe(Collections.singletonList("my-topic"));
-                while(true) {
-                    ConsumerRecords<String, String> records = consumer.poll(2000);
-                    for (ConsumerRecord<String, String> record : records)
-                        LOGGER.info("offset = {}, key = {}, value = {}", record.offset(), record.key(), record.value());
-                }
-        });
+    private Producer<String,String> buildProducer(KafkaSimpleConfiguration configuration){
+        return new KafkaProducer<>(configuration.getProducer());
     }
+
+    private Consumer<String,String> buildConsumer(KafkaSimpleConfiguration configuration){
+        Consumer<String,String> consumer = new KafkaConsumer<>(configuration.getConsumer());
+        consumer.subscribe(Collections.singletonList("my-topic"));
+        return consumer;
+    }
+
 }
--- src/main/java/com/kafka/resources/KafkaResource.java
+++ src/main/java/com/kafka/resources/KafkaResource.java
@@ -1,5 +1,6 @@
 package com.kafka.resources;
 
+import java.time.Duration;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -9,6 +10,14 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.codahale.metrics.annotation.Timed;
 import com.kafka.api.Saying;
 
@@ -16,14 +25,20 @@ import com.kafka.api.Saying;
 @Produces(MediaType.APPLICATION_JSON)
 public class KafkaResource {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaResource.class);
+
     private final String template;
     private final String defaultName;
     private final AtomicLong counter;
+    private final Producer<String,String> kafkaProducerBean;
+    private final Consumer<String,String> kafkaConsumerBean;
 
-    public KafkaResource(String template, String defaultName) {
+    public KafkaResource(String template, String defaultName, Producer<String,String> producer, Consumer<String,String> consumer) {
         this.template = template;
         this.defaultName = defaultName;
         this.counter = new AtomicLong();
+        this.kafkaProducerBean = producer;
+        this.kafkaConsumerBean = consumer;
     }
 
     @GET
@@ -34,6 +49,25 @@ public class KafkaResource {
     }
 
 
+    @GET
+    @Path("/producer")
+    @Timed
+    public Saying sendMessage(@QueryParam("message") String message) {
+        kafkaProducerBean.send(new ProducerRecord<>("my-topic", message, message));
+        return new Saying(counter.incrementAndGet(), message);
+    }
 
+    @GET
+    @Path("/consumer")
+    @Timed
+    public Saying getMessage() {
+        ConsumerRecords<String, String> records = kafkaConsumerBean.poll(Duration.ofMinutes(3));
+        for (ConsumerRecord<String, String> record : records) {
+            LOGGER.info("offset = {}, key = {}, value = {}", record.offset(), record.key(), record.value());
+            LOGGER.info("topic = {}", record.topic());
+            LOGGER.info(record.toString());
+        }
+        return new Saying(counter.incrementAndGet(), records.toString());
+    }
 
 }
